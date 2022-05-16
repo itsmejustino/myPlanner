3df0993b9f9c4fb051672452baef121fe065831d
Justino <justindortiz@me.com>
2022-05-15 22:30:53 -0600

    pseudo-code

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// create a clock function  
+//assign given variable that will track a running clock with moment .js
+//dynamically create time boxes that have a text field from 8am-4pm. 
+//call a function that will change the color of the current time block. Time block will turn a color at the end of every hour to indicate the time has passed. Current time will be indicated with a light color. Time blocks in the future by 1 hour will be a light color. 
+// add save button to each timeblock. onclick the button will store information to local storage using current text input. 
+//track input to local storage and print underneath the jumbotron 

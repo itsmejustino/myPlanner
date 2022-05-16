a210f9a0eefe1373a9aae5fba668fcf4b6be877a
Justino <justindortiz@me.com>
2022-05-15 23:17:03 -0600

    set variables for time and assign to html tag

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,21 @@
-// create a clock function  
+// create a clock function
 //assign given variable that will track a running clock with moment .js
-//dynamically create time boxes that have a text field from 8am-4pm. 
-//call a function that will change the color of the current time block. Time block will turn a color at the end of every hour to indicate the time has passed. Current time will be indicated with a light color. Time blocks in the future by 1 hour will be a light color. 
-// add save button to each timeblock. onclick the button will store information to local storage using current text input. 
-//track input to local storage and print underneath the jumbotron 
+//dynamically create time boxes that have a text field from 8am-4pm.
+//call a function that will change the color of the current time block. Time block will turn a color at the end of every hour to indicate the time has passed. Current time will be indicated with a light color. Time blocks in the future by 1 hour will be a light color.
+// add save button to each timeblock. onclick the button will store information to local storage using current text input.
+//track input to local storage and print underneath the jumbotron
+
+//clock function. call on element id #time-clock in jumbotron and assign a let vaariable. assign function to call on the variable in jumbotron to print timeclock and update in a loop. set timeclock to display interval to update timer every second. Do the same to track date in readable format. set to update every second to stay current with time.
+
+let timeClock = document.getElementById('time-clock')
+let dateTracker = document.getElementById('date-tracker')
+
+window.setInterval(() => {
+  $(timeClock).html(moment().format('h:mm:ss'))
+}, 1000)
+
+window.setInterval(() => {
+  $(dateTracker).html(moment().format('ddd MM/DD/Y'))
+}, 1000)
+
+//dynamically create time boxes that have a text field from 8am-4pm.
